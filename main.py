86417a33d761fadf938cd264ccd3a9aaf208269c
Ericw <eweissm1@asu.edu>
2024-10-16 19:37:09 -0700

    new commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #TODO:
 # cold draw stepper stops randomly on jogs down.
-
+# some twist imparted during FR
+# FR randomly stops and must be restarted manually
+# FR spacing a little small (we wanted 7mm but I got 5.5-6.5
+# Writhe formula is Wrong
 
 import tkinter as tk
 import serial
